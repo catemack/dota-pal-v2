42119004c8597f5d379c19b3ac21ce9308843a1f
rhint <rswanson5@gmail.com>
2015-03-28 16:20:39 -0700

    Detail parser

--- app/src/main/java/io/github/riijen/dotapal_v2/model/Item.java
+++ app/src/main/java/io/github/riijen/dotapal_v2/model/Item.java
@@ -5,12 +5,23 @@ package io.github.riijen.dotapal_v2.model;
  */
 public enum Item {
 
+    BLINKDAGGER ("blink", "1", "Blink Dagger"),
+    BLADESOFATTACK ("blades_of_attack", "2", "Blades Of Attack"),
+    BROADSWORD ("broadsword", "3", "Broadsword"),
+    CHAINMAIL ("chainmail", "4", "Chainmail"),
+    CLAYMORE ("claymore", "5", "Claymore");
+
+
+
+
     private final String name;
     private final String id;
     private final String localizedName;
 
     Item(String name, String id, String localizedName) {
-
+        this.name = name;
+        this.id = id;
+        this.localizedName = localizedName;
     }
 
 }
