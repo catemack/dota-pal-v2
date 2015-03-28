e530c82b2e1eb70f9cabd8d78cc50521ddb8f564
rhint <rswanson5@gmail.com>
2015-03-28 13:45:11 -0700

    Updated parser with model implementation

--- app/src/main/java/io/github/riijen/dotapal_v2/parser/HttpsRequest.java
+++ app/src/main/java/io/github/riijen/dotapal_v2/parser/HttpsRequest.java
@@ -7,5 +7,20 @@ public class HttpsRequest {
 
     private String STEAM_KEY = "29A1879DE34083B387041CE96FBA45E7";
 
+    /**
+     * Jandrix account_id for testing purposes
+     */
     private String JANDRIX_ID = "4841978";
+
+    public String generateGeneralRequest(String account_id) {
+        return "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?" +
+                "account_id=" + account_id +
+                "&key=" + STEAM_KEY;
+    }
+
+    public String generateRequestByMatchId(String match_id, String account_id) {
+        return "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?" +
+                "match_id=" + match_id +
+                "&key=" + STEAM_KEY;
+    }
 }
--- app/src/main/java/io/github/riijen/dotapal_v2/parser/Parser.java
+++ app/src/main/java/io/github/riijen/dotapal_v2/parser/Parser.java
@@ -5,8 +5,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import io.github.riijen.dotapal_v2.model.Match;
+import io.github.riijen.dotapal_v2.model.Player;
 
 public class Parser {
 
@@ -15,27 +20,57 @@ public class Parser {
      * @param rawJson - Raw JSON text buffered from dota 2 api
      *                using account_id - for Jandrix
      */
-    public Set<Integer> parseByAccountId(String rawJson) {
+    public List<Match> parseByAccountId(String rawJson) {
 
-        Set<Integer> matchIds = new HashSet<Integer>();
+        List<Match> allMatches = null;
         JSONTokener convertedJson = new JSONTokener(rawJson);
 
         try {
             JSONObject jObject = new JSONObject(convertedJson);
             JSONObject result = jObject.getJSONObject("result");
+
+            int status = result.getInt("status");
+
+            if (status == 15) {
+                return allMatches;
+            }
+
+            allMatches = new ArrayList<Match>();
+
             JSONArray matches = result.getJSONArray("matches");
 
             for (int i = 0 ; i < 10 ; i++) {
-                JSONObject match = matches.getJSONObject(i);
-                Integer matchId = match.getInt("match_id");
-                matchIds.add(matchId);
+
+                List<Player> radiant = new ArrayList<Player>();
+                List<Player> dire = new ArrayList<Player>();
+
+                JSONObject jmatch = matches.getJSONObject(i);
+
+                String matchId = jmatch.getString("match_id");
+                JSONArray players = jmatch.getJSONArray("players");
+
+                for (int j = 0 ; j < players.length() ; j++) {
+                    JSONObject player = players.getJSONObject(i);
+                    String account_id = player.getString("account_id");
+                    String hero_id = player.getString("hero_id");
+                    int player_slot = player.getInt("player_slot");
+
+                    if (player_slot < 128) {
+                        radiant.add(new Player(account_id, hero_id));
+                    } else {
+                        dire.add(new Player(account_id, hero_id));
+                    }
+                }
+
+                Match match = new Match(matchId, radiant, dire);
+                allMatches.add(match);
             }
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        return matchIds;
+        return allMatches;
     }
 
     /**
@@ -43,7 +78,20 @@ public class Parser {
      * @param rawJson
      */
 
-    public void parseMatchDetails(String rawJson) {
+    public void parseMatchDetails(String rawJson, String account_id) {
+
+        JSONTokener convertedJson = new JSONTokener(rawJson);
+
+        try {
+            JSONObject jObject = new JSONObject(convertedJson);
+            JSONObject result = jObject.getJSONObject("result");
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         // TODO
 
