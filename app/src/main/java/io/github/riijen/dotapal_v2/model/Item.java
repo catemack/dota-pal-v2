913aa3e0059e231f987d08612c994bdcd671423d
rhint <rswanson5@gmail.com>
2015-03-28 16:13:11 -0700

    Detail parser

---
+++ app/src/main/java/io/github/riijen/dotapal_v2/model/Item.java
@@ -0,0 +1,16 @@
+package io.github.riijen.dotapal_v2.model;
+
+/**
+ * Created by Ryan on 15-03-28.
+ */
+public enum Item {
+
+    private final String name;
+    private final String id;
+    private final String localizedName;
+
+    Item(String name, String id, String localizedName) {
+
+    }
+
+}
