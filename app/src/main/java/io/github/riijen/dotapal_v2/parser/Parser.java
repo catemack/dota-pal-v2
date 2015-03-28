80dac47d0b0ebfe0da5e4092d7b2af130e4402d3
rhint <rswanson5@gmail.com>
2015-03-28 12:07:38 -0700

    some more changes

--- app/src/main/java/io/github/riijen/dotapal_v2/parser/Parser.java
+++ app/src/main/java/io/github/riijen/dotapal_v2/parser/Parser.java
@@ -1,7 +1,41 @@
 package io.github.riijen.dotapal_v2.parser;
 
-/**
- * Created by kanghee on 3/28/2015.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Parser {
+
+    /**
+     * Returns the 10 most recently played games from a given account_id
+     * @param rawJson - Raw JSON text buffered from dota 2 api
+     *                using account_id - for Jandrix
+     */
+    public Set<Integer> parseByAccountId(String rawJson) {
+
+        Set<Integer> matchIds = new HashSet<Integer>();
+        JSONTokener convertedJson = new JSONTokener(rawJson);
+
+        try {
+            JSONObject jObject = new JSONObject(convertedJson);
+            JSONObject result = jObject.getJSONObject("result");
+            JSONArray matches = result.getJSONArray("matches");
+
+            for (int i = 0 ; i < 10 ; i++) {
+                JSONObject match = matches.getJSONObject(i);
+                Integer matchId = match.getInt("match_id");
+                matchIds.add(matchId);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return matchIds;
+    }
+
 }
